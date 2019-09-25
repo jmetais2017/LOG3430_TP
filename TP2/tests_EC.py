bf6566499ac221abf7d3c39cf850403adbb9da50
Ivan <ivanmolod@gmail.com>
2019-09-25 15:09:35 -0400

    fix erreurs fichiers test

---
+++ TP2/tests_EC.py
@@ -0,0 +1,41 @@
+import unittest
+import unittest.mock
+import os
+import generators 
+
+class TestGraphEC(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_simple(self):
+
+        for nbVertices in range(1, 10):
+            for nbEdges in range(1, nbVertices):
+                graph = generators.simple(nbVertices, nbEdges)
+                self.assertEqual(graph.V(), nbVertices)
+                self.assertEqual(graph.E(), nbEdges)
+
+
+        pass
+
+    def test_simple_with_probability(self):
+        pass
+
+    def test_bipartite(self):
+        pass
+
+    def test_bipartite_with_probability(self):
+        pass
+
+    def test_eulerian_cycle(self):
+        pass
+
+    def test_regular(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

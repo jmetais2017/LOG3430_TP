4190adc45f513a1376a6b3ebf2ca1762c7eb149d
Ivan <ivanmolod@gmail.com>
2019-10-09 12:34:49 -0400

    ajout de commentaires pour les tests

--- TP3/TestQueue.py
+++ TP3/TestQueue.py
@@ -12,6 +12,7 @@ class TestQueue(unittest.TestCase):
     def tearDown(self):
         pass
 
+    #tests pour une queue vide
     def testEmptyQueue(self):
 
         queue = Queue()
@@ -21,19 +22,28 @@ class TestQueue(unittest.TestCase):
         self.assertFalse(queue.isFull())
         self.assertEqual(queue.size(), 0)
 
+    #tests permettant de verifier qu'une queue vide lance une erreur
+    #si on essaie d'obtenir le premier ou le dernier element
     def testEmptyQueueRaisesValueErrorWhenCheckingFirstAndLast(self):
         self.assertRaises(ValueError, Queue().check_first)
         self.assertRaises(ValueError, Queue().check_last)
 
+    #test permettant de verifier qu'une queue vide lance une erreur
+    #si on essaie de retirer un element
     def testEmptyQueueRaisesValueErorWhenDequeuing(self):
         self.assertRaises(ValueError, Queue().dequeue)
 
+    #test permettant de verifier qu'une queue avec un element n'est pas
+    #consideree a avoir un dernier element et qu'elle lance une erreur
+    #si on l'essaie d'obtenir
     def testQueueWithOneElementRaisesValueErrorWhenLastIsChecked(self):
         queue = Queue()
         queue.enqeue(1)
         self.assertTrue(queue.hasOne())
         self.assertRaises(ValueError, queue.check_last)
 
+    #test permettant de verifier qu'il est possible d'ajouter des elements
+    #tant que ca ne depasse pas la capacite maximale permise
     def testEnqeueToMaxDoesNotThrowErrors(self):
 
         queue = Queue()
@@ -43,10 +53,16 @@ class TestQueue(unittest.TestCase):
             self.assertFalse(queue.isFull())
             queue.enqeue(k)
             self.assertEqual(queue.size(), k+1)
+            self.assertEqual(str(queue.check_first()), str(0))
             self.assertEqual(str(queue.check_last()), str(k))
 
+        #on s'assure que la pile est considere pleine si on a
+        #mis la quantite maximale d'elements permis
         self.assertTrue(queue.isFull())
 
+    #test permettant de verifier que la queue lance une erreur
+    #si on essaie d'ajouter un element alors que la nombre maximal
+    #d'elements est deja atteint
     def testEnqueueMoreThanMaxRaisesValueError(self):
 
         queue = Queue()
@@ -56,7 +72,10 @@ class TestQueue(unittest.TestCase):
 
         self.assertRaises(ValueError, queue.enqeue, 0)
 
-    def testDequeueReturnsExpectedValues(self):
+    #test peremettant de s'assurer que les fonctions check_first
+    #et deqeue fonctionnent comme prevu alors que la pile se fait
+    #depiler
+    def testCheckFirstAndDequeueReturnsExpectedValues(self):
 
         queue = Queue()
 
@@ -65,7 +84,9 @@ class TestQueue(unittest.TestCase):
 
         tailleAttendue = queue.size()
 
+        #tests pour le premier element
         self.assertEqual(str(queue.check_first()), str(0))
+        self.assertEqual(str(queue.check_last()), str(99))
         self.assertTrue(queue.isFull())
         self.assertEqual(queue.size(), tailleAttendue)
 
@@ -74,7 +95,9 @@ class TestQueue(unittest.TestCase):
         tailleAttendue = tailleAttendue - 1
         self.assertEqual(queue.size(), tailleAttendue)
 
+        #tests pour les elements intermediaires
         for k in range(1, queue.MAX-1):
+            self.assertEqual(str(queue.check_last()), str(99))
             self.assertEqual(str(queue.check_first()), str(k))
             self.assertEqual(str(queue.dequeue()), str(k))
             self.assertFalse(queue.isFull())
@@ -82,6 +105,7 @@ class TestQueue(unittest.TestCase):
             tailleAttendue = tailleAttendue - 1
             self.assertEqual(queue.size(), tailleAttendue)
 
+        #tests pour le dernier element
         self.assertEqual(queue.size(),1)
         self.assertTrue(queue.hasOne)
         self.assertRaises(ValueError, queue.check_last)

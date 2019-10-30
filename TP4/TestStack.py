c6682b5633991c8ee61688c75798cc6e65a07485
Ivan <ivanmolod@gmail.com>
2019-10-30 15:20:26 -0400

    creation du fichier de test

---
+++ TP4/TestStack.py
@@ -0,0 +1,16 @@
+import unittest
+import unittest.mock
+import os
+from Stack import Stack
+
+class TestQueue(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
